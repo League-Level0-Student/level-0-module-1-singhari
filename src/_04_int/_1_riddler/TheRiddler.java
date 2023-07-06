7f5ac767769253d811694075436d6a01d59c27e6
league <league@imac-12.attlocal.net>
2023-07-05 20:01:50 -0700

    0.1.3-0.1.4

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,24 +2,45 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle = "Which word in the dictionary is spelled incorrectly?";
+String ans = JOptionPane.showInputDialog(null,riddle);
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(ans.equals("incorrectly")) {
+	String er = "Correct";
+	JOptionPane.showMessageDialog(null,er);
+	score++;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	String er = "wrong";
+	JOptionPane.showMessageDialog(null,er);
+}
+//null means?
 		// 6. Add some more riddles
-
+//What has a neck but no head?
+String market = "What has a neck but no head?";
+String wer = JOptionPane.showInputDialog(null,market);
+if(wer.equals("a bottle")) {
+	String er = "Correct";
+	JOptionPane.showMessageDialog(null,er);
+	score++;
+}
+		
+else {
+	String er = "wrong";
+	JOptionPane.showMessageDialog(null,er);
 		// 2. Make a pop up to show the score.
 		
+	}
 	}
 }
 
--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -32,15 +32,29 @@ public class RobotInSpace extends KeyboardAdapter {
 	private void moveRobot(int keyPressed) {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
-		
+		System.out.println(keyPressed);
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-		
+		if(keyPressed == 38) {
+			rob.setAngle(0);
+			rob.move(5);
+			
+		}
 		// 4. If the down arrow is pressed, move the Robot down.
-		
+		if(keyPressed == 40) {
+			rob.setAngle(180);
+			rob.move(5);
+			
+		}
 		// 5. If the left arrow is pressed, make the Robot go left.
-		
+		if(keyPressed == 37) {
+			rob.setAngle(90);
+			rob.move(5);
+		}
 		// 6. If right is pressed, move the Robot right.
-		
+		if(keyPressed == 39) {
+			rob.setAngle(270);
+			rob.move(5);
+		}
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 		
 	}
